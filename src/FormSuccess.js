3dc6c07cddb05cca1a0f5fd4373bef38003ea080
artindesign <31039691+artindesign@users.noreply.github.com>
2020-12-04 02:14:57 +0000

    First commit

---
+++ src/FormSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FormSuccess() {
+    return (
+        <div className="form-content-right">
+            <div className="form-success">Thank you for signing up</div>
+            <img src='img/undraw_Mail_sent_re_0ofv.svg' alt="success-image" className="form-img-2" />
+        </div>
+    )
+}
+
+export default FormSuccess
